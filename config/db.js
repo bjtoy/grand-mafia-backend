3207c3762b0d1a774ff1440d4fe5e7d30d8cf483
BJ Toy <bjtoy69@gmail.com>
2026-05-20 14:42:39 +0930

    fix downgraded to prisma 6.2

--- config/db.js
+++ config/db.js
@@ -1,8 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient({
-  // Prisma 7 fallback mode — works with normal postgres:// URLs
-  engine: "binary",
-});
+const prisma = new PrismaClient();
 
 module.exports = prisma;
